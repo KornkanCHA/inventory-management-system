f82e0fb32840b6397b2f4d7c9e11e8212e995a0a
Kornkan <120338678+KornkanCHA@users.noreply.github.com>
2024-12-10 15:22:28 +0700

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Inventory Management API') 
+    .setDescription('API documentation for managing items')
+    .setVersion('1.0')
+    .build(); 
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
