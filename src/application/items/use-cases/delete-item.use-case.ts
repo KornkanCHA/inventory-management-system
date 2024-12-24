31a67f59ca068f4b66c6367ed2346d3d1602b492
Kornkan <120338678+KornkanCHA@users.noreply.github.com>
2024-12-25 04:04:57 +0700

    Improve controller response

--- src/application/items/use-cases/delete-item.use-case.ts
+++ src/application/items/use-cases/delete-item.use-case.ts
@@ -1,15 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ItemRepository } from 'src/interface-adapters/repositories/item.repository';
+import { Item } from 'src/domain/entities/item.entity';
 
 @Injectable()
 export class DeleteItemUseCase {
   constructor(private readonly itemRepository: ItemRepository) {}
 
-  async execute(id: string): Promise<void> {
-    const existingItem = await this.itemRepository.findById(id);
-    if (!existingItem) {
-      throw new NotFoundException(`Item with ID ${id} not found`);
+  async execute(id: string): Promise<Object> {
+    const item = await this.itemRepository.findById(id);
+    if (!item) {
+      throw new NotFoundException(`Item with id ${id} not found`);
     }
     await this.itemRepository.delete(id);
+    return { message: 'Delete Successful' };
   }
 }
