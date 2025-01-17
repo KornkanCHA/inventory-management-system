e433d2142234819358935bccfe655c76df47f534
Kornkan <120338678+KornkanCHA@users.noreply.github.com>
2025-01-17 07:48:19 +0700

    test: unit tests for controller

--- src/items/interface-adapters/controllers/item.controller.spec.ts
+++ src/items/interface-adapters/controllers/item.controller.spec.ts
@@ -16,133 +16,126 @@ import { Item } from 'src/items/domain/entities/item.entity';
 
 describe('ItemController', () => {
   let controller: ItemController;
-  let mockItem: Item;
+
+  const mockItem: Item = {
+    item_id: '123e4567-e89b-12d3-a456-426614174000',
+    name: 'Mock Item',
+    description: 'This is a mock item',
+    quantity: 10,
+    borrowedQuantity: 2,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  };
+
+  const mockCreateItemDto: CreateItemDto = {
+    name: 'New Item',
+    quantity: 5,
+    description: 'New item description',
+  };
+
+  const mockUpdateItemDto: UpdateItemDto = {
+    name: 'Updated Item',
+    quantity: 15,
+    description: 'Updated description',
+  };
+
+  const mockBorrowItemDto: BorrowItemDto = {
+    quantity: 2,
+  };
+
+  const mockReturnItemDto: ReturnItemDto = {
+    quantity: 2,
+  };
+
+  const mockItemUseCase = {
+    execute: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemController],
       providers: [
-        { provide: CreateItemUseCase, useValue: { execute: jest.fn() } },
-        { provide: GetItemsUseCase, useValue: { execute: jest.fn() } },
-        { provide: GetItemByIdUseCase, useValue: { execute: jest.fn() } },
-        { provide: UpdateItemUseCase, useValue: { execute: jest.fn() } },
-        { provide: DeleteItemUseCase, useValue: { execute: jest.fn() } },
-        { provide: SearchItemUseCase, useValue: { execute: jest.fn() } },
-        { provide: BorrowItemUseCase, useValue: { execute: jest.fn() } },
-        { provide: ReturnItemUseCase, useValue: { execute: jest.fn() } },
+        { provide: CreateItemUseCase, useValue: mockItemUseCase },
+        { provide: UpdateItemUseCase, useValue: mockItemUseCase },
+        { provide: GetItemsUseCase, useValue: mockItemUseCase },
+        { provide: GetItemByIdUseCase, useValue: mockItemUseCase },
+        { provide: DeleteItemUseCase, useValue: mockItemUseCase },
+        { provide: SearchItemUseCase, useValue: mockItemUseCase },
+        { provide: BorrowItemUseCase, useValue: mockItemUseCase },
+        { provide: ReturnItemUseCase, useValue: mockItemUseCase },
       ],
     }).compile();
 
     controller = module.get<ItemController>(ItemController);
-
-    mockItem = {
-      item_id: '123e4567-e89b-12d3-a456-426614174000',
-      name: 'Sample Item',
-      description: "Test Item",
-      quantity: 10,
-      borrowedQuantity: 0,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    } as Item;
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('create', () => {
-    it('should create an item', async () => {
-      const createItemDto: CreateItemDto = { name: 'Test Item', quantity: 5 };
-      jest.spyOn(controller['createItemUseCase'], 'execute').mockResolvedValue(mockItem);
-
-      const result = await controller.create(createItemDto);
-
-      expect(result).toEqual(mockItem);
-      expect(controller['createItemUseCase'].execute).toHaveBeenCalledWith(createItemDto);
-    });
-  });
-
   describe('findAll', () => {
     it('should return all items', async () => {
-      jest.spyOn(controller['getItemsUseCase'], 'execute').mockResolvedValue([mockItem]);
-
+      mockItemUseCase.execute.mockResolvedValue([mockItem]);
       const result = await controller.findAll();
-
       expect(result).toEqual([mockItem]);
-      expect(controller['getItemsUseCase'].execute).toHaveBeenCalled();
     });
   });
 
   describe('findById', () => {
     it('should return an item by ID', async () => {
-      jest.spyOn(controller['getItemByIdUseCase'], 'execute').mockResolvedValue(mockItem);
-
+      mockItemUseCase.execute.mockResolvedValue(mockItem);
       const result = await controller.findById(mockItem.item_id);
-
       expect(result).toEqual(mockItem);
-      expect(controller['getItemByIdUseCase'].execute).toHaveBeenCalledWith(mockItem.item_id);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new item', async () => {
+      mockItemUseCase.execute.mockResolvedValue(mockItem);
+      const result = await controller.create(mockCreateItemDto);
+      console.log(result)
+      console.log(mockCreateItemDto)
+      expect(result).toEqual(mockItem);
     });
   });
 
   describe('update', () => {
-    it('should update an item', async () => {
-      const updateItemDto: UpdateItemDto = { name: 'Updated Item', quantity: 15 };
-      jest.spyOn(controller['updateItemUseCase'], 'execute').mockResolvedValue(mockItem);
-
-      const result = await controller.update(mockItem.item_id, updateItemDto);
-
+    it('should update an item by ID', async () => {
+      mockItemUseCase.execute.mockResolvedValue(mockItem);
+      const result = await controller.update(mockItem.item_id, mockUpdateItemDto);
       expect(result).toEqual(mockItem);
-      expect(controller['updateItemUseCase'].execute).toHaveBeenCalledWith(mockItem.item_id, updateItemDto);
     });
   });
 
   describe('delete', () => {
-    it('should delete an item', async () => {
-      jest.spyOn(controller['deleteItemUseCase'], 'execute').mockResolvedValue({ message: 'Deleted' });
-
+    it('should delete an item by ID', async () => {
+      mockItemUseCase.execute.mockResolvedValue({ message: 'Item deleted successfully' });
       const result = await controller.delete(mockItem.item_id);
+      expect(result).toEqual({ message: 'Item deleted successfully' });
+    });
+  });
 
-      expect(result).toEqual({ message: 'Deleted' });
-      expect(controller['deleteItemUseCase'].execute).toHaveBeenCalledWith(mockItem.item_id);
+  describe('search', () => {
+    it('should search items by query', async () => {
+      mockItemUseCase.execute.mockResolvedValue([mockItem]);
+      const result = await controller.search('mock', 'name', 'ASC');
+      expect(result).toEqual([mockItem]);
     });
   });
 
   describe('borrow', () => {
     it('should borrow an item', async () => {
-      const borrowItemDto: BorrowItemDto = { quantity: 2 };
-      jest.spyOn(controller['borrowItemUseCase'], 'execute').mockResolvedValue(mockItem);
-
-      const result = await controller.borrow(mockItem.item_id, borrowItemDto);
-
+      mockItemUseCase.execute.mockResolvedValue(mockItem);
+      const result = await controller.borrow(mockItem.item_id, mockBorrowItemDto);
       expect(result).toEqual(mockItem);
-      expect(controller['borrowItemUseCase'].execute).toHaveBeenCalledWith(mockItem.item_id, borrowItemDto);
     });
   });
 
   describe('return', () => {
-    it('should return an item', async () => {
-      const returnItemDto: ReturnItemDto = { quantity: 2 };
-      jest.spyOn(controller['returnItemUseCase'], 'execute').mockResolvedValue(mockItem);
-
-      const result = await controller.return(mockItem.item_id, returnItemDto);
-
+    it('should return a borrowed item', async () => {
+      mockItemUseCase.execute.mockResolvedValue(mockItem);
+      const result = await controller.return(mockItem.item_id, mockReturnItemDto);
       expect(result).toEqual(mockItem);
-      expect(controller['returnItemUseCase'].execute).toHaveBeenCalledWith(mockItem.item_id, returnItemDto);
-    });
-  });
-
-  describe('search', () => {
-    it('should search for items', async () => {
-      const query = 'Sample';
-      const sortBy = 'name';
-      const order: 'ASC' | 'DESC' = 'ASC';
-
-      jest.spyOn(controller['searchItemUseCase'], 'execute').mockResolvedValue([mockItem]);
-
-      const result = await controller.search(query, sortBy, order);
-
-      expect(result).toEqual([mockItem]);
-      expect(controller['searchItemUseCase'].execute).toHaveBeenCalledWith(query, sortBy, order);
     });
   });
 });
