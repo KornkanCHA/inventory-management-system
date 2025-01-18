8e1d9132856e5b0919f1f16165761e434a895003
Kornkan <120338678+KornkanCHA@users.noreply.github.com>
2025-01-18 09:24:31 +0700

    style: remove full stop

--- init.sql
+++ init.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS items (
 
 INSERT INTO items (item_id, name, description, quantity, borrowedQuantity)
 VALUES
-    ('123e4567-e89b-12d3-a456-426614174000', 'Monitor', 'A 27-inch 4K monitor.', 7, 0),
+    ('123e4567-e89b-12d3-a456-426614174000', 'Monitor', 'A 27-inch 4K monitor', 7, 0),
     ('550e8400-e29b-41d4-a716-446655440000', 'Mouse', 'A wireless mouse', 5, 1),
-    ('9f8c4e3a-b2df-4c1e-9a33-dfb3e1b2c5f0', 'Microphone', 'A high-quality condenser microphone.', 6, 3),
+    ('9f8c4e3a-b2df-4c1e-9a33-dfb3e1b2c5f0', 'Microphone', 'A high-quality condenser microphone', 6, 3),
     ('f47ac10b-58cc-4372-a567-0e02b2c3d479', 'Laptop', 'A gaming laptop', 10, 2); 
