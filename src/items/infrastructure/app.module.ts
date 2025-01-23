a8c5d0ec819a8b868205247427897e560bedcb1a
Kornkan <120338678+KornkanCHA@users.noreply.github.com>
2025-01-23 08:48:25 +0700

    refactor: add layer infra

---
+++ src/items/infrastructure/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemsModule } from '../application/use-cases/item.use-cases.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
+
+/**
+ * The main module for the application.
+ * @description This module imports TypeOrm configuration for the application.
+ */
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => typeOrmConfig(configService),
+      inject: [ConfigService],
+    }),
+    ItemsModule,
+  ],
+})
+export class AppModule {}
